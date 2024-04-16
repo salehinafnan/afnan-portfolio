2481aa74fa186956e73ac26f2a6f0b3ce1f86b9d
Mushfiqus Salehin Afnan <salehinafnan@gmail.com>
2024-04-17 05:08:05 +0600

    minor

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import Technologies from "./components/Technologies";
 
 const App = () => {
   return (
-    <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-slate-700 selection:text-white">
+    <div className="overflow-x-hidden text-neutral-300 antialiased">
       <div className="fixed top-0 -z-10 h-full w-full">
         <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
       </div>
