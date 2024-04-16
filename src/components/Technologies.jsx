dcab4b57afed20cc8fa104dba7bf2cdb8c688698
Mushfiqus Salehin Afnan <salehinafnan@gmail.com>
2024-04-16 19:07:18 +0600

    minor details

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -12,26 +12,67 @@ const Technologies = () => {
       <h1 className="my-20 text-center text-4xl">Technologies</h1>
       <div className="flex flex-wrap items-center justify-center gap-4">
         <div className="rounded-2xl border-4 border-neutral-800 p-4">
-          <RiReactjsLine className="text-6xl text-cyan-500" />
+          <a
+            href="https://reactjs.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <RiReactjsLine className="text-6xl text-cyan-500" />
+          </a>
         </div>
         <div className="rounded-2xl border-4 border-neutral-800 p-4">
-          <TbBrandNextjs className="text-6xl" />
+          <a
+            href="https://nextjs.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <TbBrandNextjs className="text-6xl" />
+          </a>
         </div>
         <div className="rounded-2xl border-4 border-neutral-800 p-4">
-          <BiLogoMongodb className="text-6xl text-green-500" />
+          <a
+            href="https://www.mongodb.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <BiLogoMongodb className="text-6xl text-green-500" />
+          </a>
         </div>
         <div className="rounded-2xl border-4 border-neutral-800 p-4">
-          <SiExpress className="text-6xl " />
+          <a
+            href="https://expressjs.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <SiExpress className="text-6xl " />
+          </a>
         </div>
         <div className="rounded-2xl border-4 border-neutral-800 p-4">
-          <BiLogoNodejs className="text-6xl text-green-500" />
+          <a
+            href="https://nodejs.org/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <BiLogoNodejs className="text-6xl text-green-500" />
+          </a>
         </div>
         <div className="rounded-2xl border-4 border-neutral-800 p-4">
-          <SiMysql className="text-6xl text-blue-500" />
+          <a
+            href="https://www.mysql.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <SiMysql className="text-6xl text-blue-500" />
+          </a>
         </div>
         <div className="rounded-2xl border-4 border-neutral-800 p-4">
-          <SiPostman className="text-6xl text-orange-500" />{" "}
-          {/* Add the Postman icon */}
+          <a
+            href="https://www.postman.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <SiPostman className="text-6xl text-orange-500" />
+          </a>
         </div>
       </div>
     </div>
