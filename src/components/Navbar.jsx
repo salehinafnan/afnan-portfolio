fa62eb9d8db2f13f1b3a4d97ae8a14f6c57853a7
Mushfiqus Salehin Afnan <salehinafnan@gmail.com>
2024-04-17 05:10:43 +0600

    minor

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { IoDocumentTextOutline } from "react-icons/io5";
 
 const Navbar = () => {
   return (
-    <nav className="my-2 mb-20 flex items-center justify-between py-4">
+    <nav className="my-4 mb-20 flex items-center justify-between py-4">
       <div className="flex flex-shrink-0 items-center ml-2">
         <img style={{ maxHeight: "26px" }} src={logo} alt="logo" />
       </div>
