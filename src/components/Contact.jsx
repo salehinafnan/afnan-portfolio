f22fc5d8ebcb2aa352a561c6936056d27a3c0461
Mushfiqus Salehin Afnan <salehinafnan@gmail.com>
2024-04-17 15:47:12 +0600

    all the animations are added and optimized

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,26 +1,42 @@
 import { useForm, ValidationError } from "@formspree/react";
+import { motion } from "framer-motion";
 
 function Contact() {
   const [state, handleSubmit] = useForm("mwkgwdea");
 
   if (state.succeeded) {
     return (
-      <p className="my-20 text-center font-medium text-2xl">
+      <motion.p
+        whileInView={{ opacity: 1, y: 0 }}
+        initial={{ opacity: 0, y: -100 }}
+        transition={{ duration: 1 }}
+        className="my-20 text-center font-medium text-2xl"
+      >
         Thanks for Contacting
-      </p>
+      </motion.p>
     );
   }
 
   return (
     <div className="pb-20">
-      <h2 className="my-10 text-center text-4xl">Get in Touch</h2>
+      <motion.h2
+        whileInView={{ opacity: 1, y: 0 }}
+        initial={{ opacity: 0, y: -100 }}
+        transition={{ duration: 1 }}
+        className="my-10 text-center text-4xl"
+      >
+        Get in Touch
+      </motion.h2>
       <div className="text-center tracking-tighter">
         <form
           action="https://formspree.io/f/mwkgwdea"
           onSubmit={handleSubmit}
           className="my-2 flex flex-col px-10 justify-center items-center"
         >
-          <input
+          <motion.input
+            whileInView={{ opacity: 1, y: 0 }}
+            initial={{ opacity: 0, y: -100 }}
+            transition={{ duration: 1 }}
             id="email"
             type="email"
             name="email"
@@ -28,7 +44,10 @@ function Contact() {
             placeholder="Your Email"
           />
           <ValidationError prefix="Email" field="email" errors={state.errors} />
-          <textarea
+          <motion.textarea
+            whileInView={{ opacity: 1, y: 0 }}
+            initial={{ opacity: 0, y: -100 }}
+            transition={{ duration: 1 }}
             id="message"
             name="message"
             className="w-full lg:w-1/4 mb-4 rounded bg-neutral-900 px-2 py-8 font-medium text-blue-100"
@@ -40,13 +59,16 @@ function Contact() {
             field="message"
             errors={state.errors}
           />
-          <button
+          <motion.button
+            whileInView={{ opacity: 1, y: 0 }}
+            initial={{ opacity: 0, y: -100 }}
+            transition={{ duration: 1 }}
             type="submit"
             disabled={state.submitting}
             className="rounded bg-neutral-900 px-5 py-2 font-medium text-blue-100"
           >
             Send
-          </button>
+          </motion.button>
         </form>
       </div>
     </div>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,27 +1,47 @@
 import { useState } from "react";
 import { PROJECTS } from "../constants";
 import { FiExternalLink } from "react-icons/fi";
+import { motion } from "framer-motion";
 
 function Projects() {
   const [selectedImage, setSelectedImage] = useState(null);
 
   return (
     <div className="pb-4">
-      <h2 className="my-20 text-center text-4xl">Projects</h2>
+      <motion.h2
+        whileInView={{ opacity: 1, y: 0 }}
+        initial={{ opacity: 0, y: -100 }}
+        transition={{ duration: 0.5 }}
+        className="my-20 text-center text-4xl"
+      >
+        Projects
+      </motion.h2>
       <div>
         {PROJECTS.map((project, index) => (
           <div key={index} className="mb-8 flex flex-wrap lg:justify-center">
-            <div className="w-full lg:w-1/4">
-              <img
+            <motion.div
+              whileInView={{ opacity: 1, x: 0 }}
+              initial={{ opacity: 0, x: -100 }}
+              transition={{ duration: 1 }}
+              className="w-full lg:w-1/4"
+            >
+              <motion.img
                 src={project.image}
                 width={200}
                 height={200}
                 alt={project.title}
                 className="mb-6 rounded"
                 onClick={() => setSelectedImage(project.image)}
+                whileHover={{ scale: 1.1 }}
+                transition={{ type: "spring", stiffness: 300 }}
               />
-            </div>
-            <div className="w-full max-w-xl lg:w-3/4">
+            </motion.div>
+            <motion.div
+              whileInView={{ opacity: 1, x: 0 }}
+              initial={{ opacity: 0, x: 100 }}
+              transition={{ duration: 1 }}
+              className="w-full max-w-xl lg:w-3/4"
+            >
               <h6 className="mb-2 font-semibold">
                 <a href={project.url} target="_blank" rel="noopener noreferrer">
                   {project.title}
@@ -41,7 +61,7 @@ function Projects() {
                   </span>
                 ))}
               </div>
-            </div>
+            </motion.div>
           </div>
         ))}
       </div>
