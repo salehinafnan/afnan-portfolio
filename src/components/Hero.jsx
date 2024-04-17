63391d8524d21d629b949ba5733e73b7b7aade59
Mushfiqus Salehin Afnan <salehinafnan@gmail.com>
2024-04-17 14:45:22 +0600

    framer motion added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,26 +2,56 @@ import { HERO_CONTENT } from "../constants";
 import profilePic from "../assets/afnanProfile.png";
 import { motion } from "framer-motion";
 
+const container = (delay) => ({
+  hidden: { x: -100, opacity: 0 },
+  visible: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      duration: 1,
+      delay: delay,
+    },
+  },
+});
+
 const Hero = () => {
   return (
     <div className="pb-4 lg:mb-35">
       <div className="flex flex-wrap">
         <div className="w-full lg:w-1/2">
           <div className="flex flex-col items-center lg:items-start text-justify">
-            <h1 className="pb-16 font-thin tracking-tight lg:mt-16 text-responsive">
+            <motion.h1
+              variants={container(0.5)}
+              initial="hidden"
+              animate="visible"
+              className="pb-16 font-thin tracking-tight lg:mt-16 text-responsive"
+            >
               Mushfiqus Salehin <span className="text-blue-100">Afnan</span>
-            </h1>
-            <span className="bg-gradient-to-r from-blue-300 via-slate-300 to-pink-300 bg-clip-text text-3xl tracking-tight text-transparent">
+            </motion.h1>
+            <motion.span
+              variants={container(1)}
+              initial="hidden"
+              animate="visible"
+              className="bg-gradient-to-r from-blue-300 via-slate-300 to-pink-300 bg-clip-text text-3xl tracking-tight text-transparent"
+            >
               Web Developer
-            </span>
-            <p className="my-2 max-w-xl py-6 font-light tracking-tighter">
+            </motion.span>
+            <motion.p
+              variants={container(1.5)}
+              initial="hidden"
+              animate="visible"
+              className="my-2 max-w-xl py-6 font-light tracking-tighter"
+            >
               {HERO_CONTENT}
-            </p>
+            </motion.p>
           </div>
         </div>
         <div className="w-full lg:w-1/2 lg:p-8">
           <div className="flex justify-center">
-            <img
+            <motion.img
+              initial={{ x: 100, opacity: 0 }}
+              animate={{ x: 0, opacity: 1 }}
+              transition={{ duration: 1, delay: 1.5 }}
               className="rounded-2xl w-full object-cover sm:object-contain md:object-scale-down"
               src={profilePic}
               alt="Mushfiqus Salehin Afnan"
