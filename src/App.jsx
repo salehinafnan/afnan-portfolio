ef872073101c70e193ccfed4f261a0cfc6cd413f
Mushfiqus Salehin Afnan <salehinafnan@gmail.com>
2024-04-26 19:25:32 +0600

    Update styles in App.jsx and Navbar.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ const App = () => {
       <div className="fixed top-0 -z-10 h-full w-full">
         <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(130%_130%_at_50%_10%,#000_50%,#191970_100%)]"></div>
       </div>
-      <div className="container mx-auto px-8 backdrop-blur-3xl backdrop-brightness-75">
+      <div className="container mx-auto px-8">
         <Navbar />
         <Hero />
         <About />
