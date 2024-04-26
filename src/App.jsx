a0483f5dfdf1011eb831d25045cf97e15242b754
Mushfiqus Salehin Afnan <salehinafnan@gmail.com>
2024-04-27 02:25:50 +0600

    Update styles in App.jsx and Navbar.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import Technologies from "./components/Technologies";
 
 const App = () => {
   return (
-    <div className="overflow-x-hidden text-neutral-300 antialiased">
+    <div className="overflow-x-hidden text-gray-300 antialiased">
       <div className="fixed top-0 -z-10 h-full w-full">
         <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(130%_130%_at_50%_10%,#000_50%,#191970_100%)]"></div>
       </div>
