d15f25488b159fb41165eb232f15280adfe6ab91
Mushfiqus Salehin Afnan <salehinafnan@gmail.com>
2024-05-13 17:11:08 +0600

    change hero and about section photos

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import { HERO_CONTENT } from "../constants";
-import profilePic from "../assets/afnanProfile.png";
+import profilePic from "../assets/afnanProfile.jpg";
 import { motion } from "framer-motion";
 
 const container = (delay) => ({
