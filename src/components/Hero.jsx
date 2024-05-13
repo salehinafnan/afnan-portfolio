79ecd1c73e3cea8cf35288809a2ac72fefcaae67
Mushfiqus Salehin Afnan <salehinafnan@gmail.com>
2024-05-13 22:50:50 +0600

    changed up the hero section animations

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -21,7 +21,7 @@ const Hero = () => {
         <div className="w-full lg:w-1/2">
           <div className="flex flex-col items-center lg:items-start text-justify">
             <motion.h1
-              variants={container(0)}
+              variants={container(0.5)}
               initial="hidden"
               animate="visible"
               className="pb-12 font-light tracking-tight lg:mt-16 text-responsive whitespace-nowrap"
@@ -32,7 +32,7 @@ const Hero = () => {
               </span>
             </motion.h1>
             <motion.span
-              variants={container(0.5)}
+              variants={container(0.75)}
               initial="hidden"
               animate="visible"
               className="bg-gradient-to-r from-blue-300 via-gray-300 to-red-300 bg-clip-text text-3xl tracking-tight text-transparent"
@@ -54,7 +54,7 @@ const Hero = () => {
             <motion.img
               initial={{ x: 100, opacity: 0 }}
               animate={{ x: 0, opacity: 1 }}
-              transition={{ duration: 0.5, delay: 1 }}
+              transition={{ duration: 1, delay: 1 }}
               className="rounded-2xl w-full object-cover sm:object-contain md:object-scale-down"
               src={profilePic}
               alt="Mushfiqus Salehin Afnan"
