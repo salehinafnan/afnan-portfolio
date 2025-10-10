0d373ed57d6f9a2469a3a987d80821ebfa6f8ac1
Mushfiqus Salehin Afnan <salehinafnan@gmail.com>
2025-10-10 19:56:22 +0600

    CV Update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -27,7 +27,7 @@ const Navbar = () => {
           </motion.div>
         </a>
         <a
-          href="https://drive.google.com/file/d/1_D7aZqK8ytZZWAmbEQEPhPS7Rv1uZaVa/view?usp=sharing"
+          href="https://drive.google.com/file/d/1_D7aZqK8ytZZWAmbEQEPhPS7Rv1uZaVa/view?usp=drive_link"
           target="_blank"
           rel="noopener noreferrer"
         >
